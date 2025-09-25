1c93f5f97543db3987286077126dc9414dc848cf
Maho Pacheco <mapach@microsoft.com>
2025-09-25 08:31:21 -0700

    Adding wwwroot

---
+++ wwwroot/js/LocalStorage.js
@@ -0,0 +1,14 @@
+window.LocalStorageActions = {
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    },
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    },
+    clearData: function () {
+        localStorage.clear();
+    }
+};
